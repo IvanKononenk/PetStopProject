6ff5c7df92f21948291960bfe84f302b4ae40589
Ivan Kononenko <vkonononenko9@gmail.com>
2024-05-10 21:21:15 +0300

    Инициализация репозитория

---
+++ TamagochiGame/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace TamagochiGame
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.progressBar2 = new System.Windows.Forms.ProgressBar();
+			this.progressBar3 = new System.Windows.Forms.ProgressBar();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(13, 13);
+			this.progressBar1.MarqueeAnimationSpeed = 10;
+			this.progressBar1.Maximum = 1000;
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(100, 23);
+			this.progressBar1.TabIndex = 0;
+			this.progressBar1.Value = 1000;
+			// 
+			// progressBar2
+			// 
+			this.progressBar2.Location = new System.Drawing.Point(13, 43);
+			this.progressBar2.MarqueeAnimationSpeed = 10;
+			this.progressBar2.Maximum = 1000;
+			this.progressBar2.Name = "progressBar2";
+			this.progressBar2.Size = new System.Drawing.Size(100, 23);
+			this.progressBar2.TabIndex = 1;
+			this.progressBar2.Value = 1000;
+			// 
+			// progressBar3
+			// 
+			this.progressBar3.Location = new System.Drawing.Point(13, 73);
+			this.progressBar3.MarqueeAnimationSpeed = 10;
+			this.progressBar3.Maximum = 1000;
+			this.progressBar3.Name = "progressBar3";
+			this.progressBar3.Size = new System.Drawing.Size(100, 23);
+			this.progressBar3.TabIndex = 2;
+			this.progressBar3.Value = 1000;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.InitialImage = null;
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(835, 475);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 3;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(859, 499);
+			this.Controls.Add(this.progressBar3);
+			this.Controls.Add(this.progressBar2);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.ProgressBar progressBar2;
+		private System.Windows.Forms.ProgressBar progressBar3;
+		private System.Windows.Forms.PictureBox pictureBox1;
+	}
+}
+
---
+++ TamagochiGame/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace TamagochiGame
+{
+	public partial class Form1 : Form
+	{
+		
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+			progressBar1.Value += 50;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			System.Timers.Timer aTimer = new System.Timers.Timer();
+			aTimer.Elapsed += Timer_Elapsed;
+			aTimer.Interval = 100;
+			aTimer.AutoReset = true;
+			aTimer.Start();
+		}
+
+		private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+		{
+			progressBar1.Value -= 1;
+			progressBar2.Value -= 1;
+			progressBar3.Value -= 1;
+		}
+	}
+}
