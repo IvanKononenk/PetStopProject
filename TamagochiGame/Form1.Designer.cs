3c074de0e8223cb24cb4eabe9dab9ebf4806f0b7
Ivan Kononenko <vkonononenko9@gmail.com>
2024-05-18 02:51:09 +0300

    alpha 0.0.1
    Added a background, Tamagochi mechanic is now working properly

--- TamagochiGame/Form1.Designer.cs
+++ TamagochiGame/Form1.Designer.cs
@@ -29,81 +29,173 @@
 		private void InitializeComponent()
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-			this.progressBar1 = new System.Windows.Forms.ProgressBar();
-			this.progressBar2 = new System.Windows.Forms.ProgressBar();
-			this.progressBar3 = new System.Windows.Forms.ProgressBar();
-			this.pictureBox1 = new System.Windows.Forms.PictureBox();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.prBarHunger = new System.Windows.Forms.ProgressBar();
+			this.prBarThirst = new System.Windows.Forms.ProgressBar();
+			this.prBarHappy = new System.Windows.Forms.ProgressBar();
+			this.picBoxPet = new System.Windows.Forms.PictureBox();
+			this.lblHunger = new System.Windows.Forms.Label();
+			this.lblThirst = new System.Windows.Forms.Label();
+			this.lblHappy = new System.Windows.Forms.Label();
+			this.btnFeed = new System.Windows.Forms.Button();
+			this.btnDrink = new System.Windows.Forms.Button();
+			this.btnPlay = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.picBoxPet)).BeginInit();
 			this.SuspendLayout();
 			// 
-			// progressBar1
+			// prBarHunger
 			// 
-			this.progressBar1.Location = new System.Drawing.Point(13, 13);
-			this.progressBar1.MarqueeAnimationSpeed = 10;
-			this.progressBar1.Maximum = 1000;
-			this.progressBar1.Name = "progressBar1";
-			this.progressBar1.Size = new System.Drawing.Size(100, 23);
-			this.progressBar1.TabIndex = 0;
-			this.progressBar1.Value = 1000;
+			this.prBarHunger.Location = new System.Drawing.Point(92, 14);
+			this.prBarHunger.MarqueeAnimationSpeed = 10;
+			this.prBarHunger.Maximum = 1000;
+			this.prBarHunger.Name = "prBarHunger";
+			this.prBarHunger.Size = new System.Drawing.Size(100, 23);
+			this.prBarHunger.TabIndex = 0;
+			this.prBarHunger.Value = 1000;
 			// 
-			// progressBar2
+			// prBarThirst
 			// 
-			this.progressBar2.Location = new System.Drawing.Point(13, 43);
-			this.progressBar2.MarqueeAnimationSpeed = 10;
-			this.progressBar2.Maximum = 1000;
-			this.progressBar2.Name = "progressBar2";
-			this.progressBar2.Size = new System.Drawing.Size(100, 23);
-			this.progressBar2.TabIndex = 1;
-			this.progressBar2.Value = 1000;
+			this.prBarThirst.Location = new System.Drawing.Point(92, 43);
+			this.prBarThirst.MarqueeAnimationSpeed = 10;
+			this.prBarThirst.Maximum = 1000;
+			this.prBarThirst.Name = "prBarThirst";
+			this.prBarThirst.Size = new System.Drawing.Size(100, 23);
+			this.prBarThirst.TabIndex = 1;
+			this.prBarThirst.Value = 1000;
 			// 
-			// progressBar3
+			// prBarHappy
 			// 
-			this.progressBar3.Location = new System.Drawing.Point(13, 73);
-			this.progressBar3.MarqueeAnimationSpeed = 10;
-			this.progressBar3.Maximum = 1000;
-			this.progressBar3.Name = "progressBar3";
-			this.progressBar3.Size = new System.Drawing.Size(100, 23);
-			this.progressBar3.TabIndex = 2;
-			this.progressBar3.Value = 1000;
+			this.prBarHappy.Location = new System.Drawing.Point(92, 72);
+			this.prBarHappy.MarqueeAnimationSpeed = 10;
+			this.prBarHappy.Maximum = 1000;
+			this.prBarHappy.Name = "prBarHappy";
+			this.prBarHappy.Size = new System.Drawing.Size(100, 23);
+			this.prBarHappy.TabIndex = 2;
+			this.prBarHappy.Value = 1000;
 			// 
-			// pictureBox1
+			// picBoxPet
 			// 
-			this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-			this.pictureBox1.InitialImage = null;
-			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(835, 475);
-			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-			this.pictureBox1.TabIndex = 3;
-			this.pictureBox1.TabStop = false;
-			this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+			this.picBoxPet.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picBoxPet.BackgroundImage")));
+			this.picBoxPet.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.picBoxPet.Cursor = System.Windows.Forms.Cursors.Default;
+			this.picBoxPet.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.picBoxPet.Image = ((System.Drawing.Image)(resources.GetObject("picBoxPet.Image")));
+			this.picBoxPet.InitialImage = null;
+			this.picBoxPet.Location = new System.Drawing.Point(0, 0);
+			this.picBoxPet.Name = "picBoxPet";
+			this.picBoxPet.Size = new System.Drawing.Size(1298, 559);
+			this.picBoxPet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.picBoxPet.TabIndex = 3;
+			this.picBoxPet.TabStop = false;
+			// 
+			// lblHunger
+			// 
+			this.lblHunger.AutoSize = true;
+			this.lblHunger.BackColor = System.Drawing.Color.Transparent;
+			this.lblHunger.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblHunger.Location = new System.Drawing.Point(24, 14);
+			this.lblHunger.Name = "lblHunger";
+			this.lblHunger.Size = new System.Drawing.Size(62, 23);
+			this.lblHunger.TabIndex = 4;
+			this.lblHunger.Text = "Голод:";
+			// 
+			// lblThirst
+			// 
+			this.lblThirst.AutoSize = true;
+			this.lblThirst.BackColor = System.Drawing.Color.Transparent;
+			this.lblThirst.Font = new System.Drawing.Font("Comic Sans MS", 12F);
+			this.lblThirst.Location = new System.Drawing.Point(21, 43);
+			this.lblThirst.Name = "lblThirst";
+			this.lblThirst.Size = new System.Drawing.Size(65, 23);
+			this.lblThirst.TabIndex = 5;
+			this.lblThirst.Text = "Жажда:";
+			// 
+			// lblHappy
+			// 
+			this.lblHappy.AutoSize = true;
+			this.lblHappy.BackColor = System.Drawing.Color.Transparent;
+			this.lblHappy.Font = new System.Drawing.Font("Comic Sans MS", 12F);
+			this.lblHappy.Location = new System.Drawing.Point(12, 72);
+			this.lblHappy.Name = "lblHappy";
+			this.lblHappy.Size = new System.Drawing.Size(74, 23);
+			this.lblHappy.TabIndex = 6;
+			this.lblHappy.Text = "Счастье:";
+			// 
+			// btnFeed
+			// 
+			this.btnFeed.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+			this.btnFeed.AutoSize = true;
+			this.btnFeed.Font = new System.Drawing.Font("Comic Sans MS", 12F);
+			this.btnFeed.Location = new System.Drawing.Point(481, 514);
+			this.btnFeed.Name = "btnFeed";
+			this.btnFeed.Size = new System.Drawing.Size(108, 33);
+			this.btnFeed.TabIndex = 7;
+			this.btnFeed.Text = "Покормить";
+			this.btnFeed.UseVisualStyleBackColor = true;
+			this.btnFeed.Click += new System.EventHandler(this.BtnFeed_Click);
+			// 
+			// btnDrink
+			// 
+			this.btnDrink.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+			this.btnDrink.AutoSize = true;
+			this.btnDrink.Font = new System.Drawing.Font("Comic Sans MS", 12F);
+			this.btnDrink.Location = new System.Drawing.Point(595, 514);
+			this.btnDrink.Name = "btnDrink";
+			this.btnDrink.Size = new System.Drawing.Size(108, 33);
+			this.btnDrink.TabIndex = 8;
+			this.btnDrink.Text = "Напоить";
+			this.btnDrink.UseVisualStyleBackColor = true;
+			this.btnDrink.Click += new System.EventHandler(this.BtnDrink_Click);
+			// 
+			// btnPlay
+			// 
+			this.btnPlay.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+			this.btnPlay.AutoSize = true;
+			this.btnPlay.Font = new System.Drawing.Font("Comic Sans MS", 12F);
+			this.btnPlay.Location = new System.Drawing.Point(709, 514);
+			this.btnPlay.Name = "btnPlay";
+			this.btnPlay.Size = new System.Drawing.Size(108, 33);
+			this.btnPlay.TabIndex = 9;
+			this.btnPlay.Text = "Поиграть";
+			this.btnPlay.UseVisualStyleBackColor = true;
+			this.btnPlay.Click += new System.EventHandler(this.BtnPlay_Click);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(859, 499);
-			this.Controls.Add(this.progressBar3);
-			this.Controls.Add(this.progressBar2);
-			this.Controls.Add(this.progressBar1);
-			this.Controls.Add(this.pictureBox1);
+			this.ClientSize = new System.Drawing.Size(1298, 559);
+			this.Controls.Add(this.btnPlay);
+			this.Controls.Add(this.btnDrink);
+			this.Controls.Add(this.btnFeed);
+			this.Controls.Add(this.lblHappy);
+			this.Controls.Add(this.lblThirst);
+			this.Controls.Add(this.lblHunger);
+			this.Controls.Add(this.prBarHappy);
+			this.Controls.Add(this.prBarThirst);
+			this.Controls.Add(this.prBarHunger);
+			this.Controls.Add(this.picBoxPet);
+			this.MinimumSize = new System.Drawing.Size(376, 400);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.Load += new System.EventHandler(this.Form1_Load);
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picBoxPet)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.ProgressBar progressBar1;
-		private System.Windows.Forms.ProgressBar progressBar2;
-		private System.Windows.Forms.ProgressBar progressBar3;
-		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.ProgressBar prBarHunger;
+		private System.Windows.Forms.ProgressBar prBarThirst;
+		private System.Windows.Forms.ProgressBar prBarHappy;
+		private System.Windows.Forms.PictureBox picBoxPet;
+		private System.Windows.Forms.Label lblHunger;
+		private System.Windows.Forms.Label lblThirst;
+		private System.Windows.Forms.Label lblHappy;
+		private System.Windows.Forms.Button btnFeed;
+		private System.Windows.Forms.Button btnDrink;
+		private System.Windows.Forms.Button btnPlay;
 	}
 }
 
--- TamagochiGame/Form1.cs
+++ TamagochiGame/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Timers;
 using System.Windows.Forms;
@@ -13,31 +14,56 @@ namespace TamagochiGame
 {
 	public partial class Form1 : Form
 	{
-		
+		int feedAmount = 50;
 		public Form1()
 		{
 			InitializeComponent();
 		}
 
-		private void pictureBox1_Click(object sender, EventArgs e)
-		{
-			progressBar1.Value += 50;
-		}
-
 		private void Form1_Load(object sender, EventArgs e)
 		{
-			System.Timers.Timer aTimer = new System.Timers.Timer();
-			aTimer.Elapsed += Timer_Elapsed;
+			System.Windows.Forms.Timer aTimer = new System.Windows.Forms.Timer();
+			aTimer.Tick += Timer_Elapsed;
 			aTimer.Interval = 100;
-			aTimer.AutoReset = true;
 			aTimer.Start();
 		}
 
-		private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+		private void Timer_Elapsed(object sender, EventArgs e)
 		{
-			progressBar1.Value -= 1;
-			progressBar2.Value -= 1;
-			progressBar3.Value -= 1;
+			int starveAmount = 1;
+
+			if (prBarHunger.Value - starveAmount < prBarHunger.Minimum)
+				prBarHunger.Value = prBarHunger.Minimum;
+			else prBarHunger.Value -= starveAmount;
+
+			if (prBarThirst.Value - starveAmount < prBarThirst.Minimum)
+				prBarThirst.Value = prBarThirst.Minimum;
+			else prBarThirst.Value -= starveAmount;
+
+			if (prBarHappy.Value - starveAmount < prBarHappy.Minimum)
+				prBarHappy.Value = prBarHappy.Minimum;
+			else prBarHappy.Value -= starveAmount;
+		}
+
+		private void BtnPlay_Click(object sender, EventArgs e)
+		{
+			if (prBarHappy.Value + feedAmount > prBarHappy.Maximum)
+				prBarHappy.Value = prBarHappy.Maximum;
+			else prBarHappy.Value += feedAmount;
+		}
+
+		private void BtnFeed_Click(object sender, EventArgs e)
+		{
+			if (prBarHunger.Value + feedAmount > prBarHunger.Maximum)
+				prBarHunger.Value = prBarHunger.Maximum;
+			else prBarHunger.Value += feedAmount;
+		}
+
+		private void BtnDrink_Click(object sender, EventArgs e)
+		{
+			if (prBarThirst.Value + feedAmount > prBarThirst.Maximum)
+				prBarThirst.Value = prBarThirst.Maximum;
+			else prBarThirst.Value += feedAmount;
 		}
 	}
 }
