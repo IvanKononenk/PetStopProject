9bbbabb585f07011276e02f1f5a3b2019ca778e1
Ivan Kononenko <vkonononenko9@gmail.com>
2024-11-07 15:29:42 +0300

    Начало работы над классом игрока

---
+++ TamagochiGame/Classes/PlayerClasses.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStop.Classes
+{
+	[Serializable]
+	public class PlayerClasses
+	{
+		public string username { get; set; }
+		public int money { get; set; }
+		public List<Pet> pets { get; set; }
+		public DateTime birthday { get; set; }
+		ДОБАВИТЬ ДНЮХУ В КОНСТРУКТОР
+		public PlayerClasses(string username, int money, List<Pet> pets)
+		{
+			this.username = username;
+			this.money = money;
+			this.pets = pets;
+		}
+	}
+}
