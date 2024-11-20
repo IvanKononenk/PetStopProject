8e35a1bc88ab2ce1bac5bcbd1fa984b47430b463
Ivan Kononenko <vkonononenko9@gmail.com>
2024-11-21 01:28:56 +0300

    Переработка отображения контейнеров(начало)

--- TamagochiGame/Forms/ChooseACrateForm.Designer.cs
+++ TamagochiGame/Forms/ChooseACrateForm.Designer.cs
@@ -28,18 +28,14 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChooseACrateForm));
 			this.LblPetName = new System.Windows.Forms.Label();
 			this.TxtPetName = new System.Windows.Forms.TextBox();
 			this.BtnConfirm = new System.Windows.Forms.Button();
 			this.BtnExit = new System.Windows.Forms.Button();
-			this.pictureBox3 = new System.Windows.Forms.PictureBox();
-			this.pictureBox2 = new System.Windows.Forms.PictureBox();
-			this.pictureBox1 = new System.Windows.Forms.PictureBox();
-			this.BtnUpdateCrates = new System.Windows.Forms.Button();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.imgListCrates = new System.Windows.Forms.ImageList(this.components);
+			this.TLPCrates = new System.Windows.Forms.TableLayoutPanel();
 			this.SuspendLayout();
 			// 
 			// LblPetName
@@ -87,65 +83,38 @@
 			this.BtnExit.Text = "Выход";
 			this.BtnExit.UseVisualStyleBackColor = true;
 			// 
-			// pictureBox3
+			// imgListCrates
 			// 
-			this.pictureBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.pictureBox3.Location = new System.Drawing.Point(439, 14);
-			this.pictureBox3.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
-			this.pictureBox3.Name = "pictureBox3";
-			this.pictureBox3.Size = new System.Drawing.Size(200, 200);
-			this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-			this.pictureBox3.TabIndex = 2;
-			this.pictureBox3.TabStop = false;
-			this.pictureBox3.Click += new System.EventHandler(this.pictureBox_Click);
+			this.imgListCrates.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+			this.imgListCrates.ImageSize = new System.Drawing.Size(16, 16);
+			this.imgListCrates.TransparentColor = System.Drawing.Color.Transparent;
 			// 
-			// pictureBox2
+			// TLPCrates
 			// 
-			this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.Top;
-			this.pictureBox2.Location = new System.Drawing.Point(227, 14);
-			this.pictureBox2.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
-			this.pictureBox2.Name = "pictureBox2";
-			this.pictureBox2.Size = new System.Drawing.Size(200, 200);
-			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-			this.pictureBox2.TabIndex = 1;
-			this.pictureBox2.TabStop = false;
-			this.pictureBox2.Click += new System.EventHandler(this.pictureBox_Click);
-			// 
-			// pictureBox1
-			// 
-			this.pictureBox1.Location = new System.Drawing.Point(15, 14);
-			this.pictureBox1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
-			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(200, 200);
-			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-			this.pictureBox1.TabIndex = 0;
-			this.pictureBox1.TabStop = false;
-			this.pictureBox1.Click += new System.EventHandler(this.pictureBox_Click);
-			// 
-			// BtnUpdateCrates
-			// 
-			this.BtnUpdateCrates.AutoSize = true;
-			this.BtnUpdateCrates.Location = new System.Drawing.Point(12, 304);
-			this.BtnUpdateCrates.Name = "BtnUpdateCrates";
-			this.BtnUpdateCrates.Size = new System.Drawing.Size(201, 33);
-			this.BtnUpdateCrates.TabIndex = 7;
-			this.BtnUpdateCrates.Text = "Изменить коробки";
-			this.BtnUpdateCrates.UseVisualStyleBackColor = true;
-			this.BtnUpdateCrates.Click += new System.EventHandler(this.BtnUpdateCrates_Click);
+			this.TLPCrates.ColumnCount = 1;
+			this.TLPCrates.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.TLPCrates.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.TLPCrates.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.TLPCrates.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.TLPCrates.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.TLPCrates.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.TLPCrates.Location = new System.Drawing.Point(12, 12);
+			this.TLPCrates.Name = "TLPCrates";
+			this.TLPCrates.RowCount = 1;
+			this.TLPCrates.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.TLPCrates.Size = new System.Drawing.Size(630, 204);
+			this.TLPCrates.TabIndex = 8;
 			// 
 			// ChooseACrateForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(654, 349);
-			this.Controls.Add(this.BtnUpdateCrates);
+			this.Controls.Add(this.TLPCrates);
 			this.Controls.Add(this.BtnExit);
 			this.Controls.Add(this.BtnConfirm);
 			this.Controls.Add(this.TxtPetName);
 			this.Controls.Add(this.LblPetName);
-			this.Controls.Add(this.pictureBox3);
-			this.Controls.Add(this.pictureBox2);
-			this.Controls.Add(this.pictureBox1);
 			this.Font = new System.Drawing.Font("Verdana", 14.25F);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
@@ -153,23 +122,17 @@
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Новый питомец";
 			this.Load += new System.EventHandler(this.ChooseACrateForm_Load);
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
 		}
 
 		#endregion
-
-		private System.Windows.Forms.PictureBox pictureBox1;
-		private System.Windows.Forms.PictureBox pictureBox2;
-		private System.Windows.Forms.PictureBox pictureBox3;
 		private System.Windows.Forms.Label LblPetName;
 		private System.Windows.Forms.TextBox TxtPetName;
 		private System.Windows.Forms.Button BtnConfirm;
 		private System.Windows.Forms.Button BtnExit;
-		private System.Windows.Forms.Button BtnUpdateCrates;
+		private System.Windows.Forms.ImageList imgListCrates;
+		private System.Windows.Forms.TableLayoutPanel TLPCrates;
 	}
 }
--- TamagochiGame/Forms/ChooseACrateForm.cs
+++ TamagochiGame/Forms/ChooseACrateForm.cs
@@ -10,7 +10,6 @@ namespace PetStop.Forms
 	public partial class ChooseACrateForm : Form
 	{
 		Random rnd = new Random();
-		PictureBox chosenBox;
 		string[] crates = JsonSerializer.Deserialize<string[]>(Resources.CratesList);
 		public ChooseACrateForm()
 		{
@@ -19,36 +18,23 @@ namespace PetStop.Forms
 
 		private void ChooseACrateForm_Load(object sender, EventArgs e)
 		{
-			pictureBox1.Image = (Bitmap)Resources.ResourceManager.GetObject(crates[rnd.Next(crates.Count())]);
-			pictureBox2.Image = (Bitmap)Resources.ResourceManager.GetObject(crates[rnd.Next(crates.Count())]);
-			pictureBox3.Image = (Bitmap)Resources.ResourceManager.GetObject(crates[rnd.Next(crates.Count())]);
-		}
-
-		private void pictureBox_Click(object sender, EventArgs e)
-		{
-			pictureBox1.Enabled = pictureBox2.Enabled = pictureBox3.Enabled = BtnUpdateCrates.Enabled = false;
-			Species.newSpecies = SpeciesFactory.speciesDictionary[SpeciesFactory.speciesDictionary.ElementAt(rnd.Next(SpeciesFactory.speciesDictionary.Count)).Key];
-			chosenBox = (PictureBox)sender;
-			object file = Resources.ResourceManager.GetObject(Species.newSpecies.listFile);
-			string[] pictures = JsonSerializer.Deserialize<string[]>((byte[])file);
-			chosenBox.Image = (Bitmap)Resources.ResourceManager.GetObject(pictures[rnd.Next(pictures.Count())]);
-			BtnConfirm.Enabled = true;
-			LblPetName.Visible = true;
-			TxtPetName.Visible = true;
-		}
-
-		private void BtnUpdateCrates_Click(object sender, EventArgs e)
-		{
-			pictureBox1.Image = (Bitmap)Resources.ResourceManager.GetObject(crates[rnd.Next(crates.Count())]);
-			pictureBox2.Image = (Bitmap)Resources.ResourceManager.GetObject(crates[rnd.Next(crates.Count())]);
-			pictureBox3.Image = (Bitmap)Resources.ResourceManager.GetObject(crates[rnd.Next(crates.Count())]);
+			PictureBox[] picBoxCrates = new PictureBox[crates.Count()];
+			for (int i = 0; i < picBoxCrates.Length; i++)
+			{
+				picBoxCrates[i] = new PictureBox
+				{
+					Image = (Bitmap)Resources.ResourceManager.GetObject(crates[i].ToString()),
+					SizeMode = PictureBoxSizeMode.Zoom,
+				};
+				TLPCrates.
+			}
 		}
 
 		private void BtnConfirm_Click(object sender, EventArgs e)
 		{
 			if (TxtPetName.Text != "")
 			{
-				Pet.activePet = new Pet(TxtPetName.Text, Species.newSpecies, chosenBox.ImageLocation, ДОДЕЛАТЬ КОНСТРУКТОР);
+
 			}
 			else
 			{
@@ -58,3 +44,12 @@ namespace PetStop.Forms
 		}
 	}
 }
+//pictureBox1.Enabled = pictureBox2.Enabled = pictureBox3.Enabled = BtnUpdateCrates.Enabled = false;
+//Species.newSpecies = SpeciesFactory.speciesDictionary[SpeciesFactory.speciesDictionary.ElementAt(rnd.Next(SpeciesFactory.speciesDictionary.Count)).Key];
+//chosenBox = (PictureBox)sender;
+//object file = Resources.ResourceManager.GetObject(Species.newSpecies.listFile);
+//string[] pictures = JsonSerializer.Deserialize<string[]>((byte[])file);
+//chosenBox.Image = (Bitmap)Resources.ResourceManager.GetObject(pictures[rnd.Next(pictures.Count())]);
+//BtnConfirm.Enabled = true;
+//LblPetName.Visible = true;
+//TxtPetName.Visible = true;
