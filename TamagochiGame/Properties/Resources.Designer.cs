02f75fc78df63d9ed87b6dc613507358ec27b821
Ivan Kononenko <vkonononenko9@gmail.com>
2024-12-03 23:24:22 +0300

    Новый спрайт

--- TamagochiGame/Properties/Resources.Designer.cs
+++ TamagochiGame/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace PetStop.Properties {
         /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap ball {
+            get {
+                object obj = ResourceManager.GetObject("ball", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap bgMainMenu {
             get {
                 object obj = ResourceManager.GetObject("bgMainMenu", resourceCulture);
